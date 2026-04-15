18443420e7632ff48e8e25bfba13b3defbf3ed10
Smatsumot0 <killer0xxxx@gmail.com>
2026-04-15 22:40:54 +0900

    link component import fix

--- app/components/ui/link/Link.tsx
+++ app/components/ui/link/Link.tsx
@@ -1,7 +1,7 @@
 import NextLink from "next/link"
 import clsx from "clsx"
 import style from "./Link.module.css"
-import buttonLikeStyle from "../../ui/button-like/button-like.module.css"
+import buttonLikeStyle from "../button-like/button-like.module.css"
 
 export type LinkProps = {
   href: string
